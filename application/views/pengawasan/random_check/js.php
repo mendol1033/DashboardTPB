b6ba132931a8f2d9aed516d400064920da5459aa
Fithoriqil Azziz <fithoriqil.azziz1033@gmail.com>
2018-09-28 10:56:35 +0700

    dropDown Tindak Lanjut

--- application/views/pengawasan/random_check/js.php
+++ application/views/pengawasan/random_check/js.php
@@ -152,7 +152,7 @@
 							'<td>'+
 							'<select class="form-control select2" name="StatusIT[]" id="StatusIT"><option value="">Pilih Status</option><option value="Y">AKTIF</option><option value="N">TIDAK AKTIF</option></select>'+
 							'</td>'+
-							'<td>'+'<select class="form-control select2" id="tindakLanjut" name="tindakLanjut[]"><option value="Patroli">Patroli</option><option value="Analisa Intelijen">Analisa</option></select>'+'</td>'+
+							'<td>'+'<select class="form-control select2" id="tindakLanjut" name="tindakLanjut[]"><option value="">Pilih Tindakan Yang Diambil</option><option value="Patroli">Patroli</option><option value="Analisa Intelijen">Analisa</option></select>'+'</td>'+
 							"<tr>"	
 							);
 					});
