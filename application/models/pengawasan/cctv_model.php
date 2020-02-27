ecbda72b98600cf36d0c0377c4e4acd73da7fe6c
fithoriqil azziz <fithoriqil.azziz1033@gmail.com>
2020-02-27 09:42:57 +0700

    sorting cctv

--- application/models/pengawasan/cctv_model.php
+++ application/models/pengawasan/cctv_model.php
@@ -15,8 +15,8 @@ class Cctv_model extends CI_Model {
 	}
 
 	var $table = "tb_cctv_detail";
-	var $column_order = array(null, 'IdPerusahaan', 'NPWP', 'NmPerusahaan', 'AlamatPabrik');
-	var $column_search = array('IdPerusahaan', 'NPWP', 'NmPerusahaan', 'AlamatPabrik');
+	var $column_order = array(null, 'NPWP', 'NmPerusahaan', 'Browser', null,'Status');
+	var $column_search = array('NPWP', 'NmPerusahaan', 'Browser','Status');
 	var $order = array('NmPerusahaan' => 'asc');
 
 	private function GetListData() {
